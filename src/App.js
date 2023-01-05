7310e733732aa7a8e34efb4e626237899d2b73a6
Zane Reid <zanecreid@hotmail.com>
2023-01-05 11:02:40 +0000

    Created the file structure

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./Containers";
+import { Cta, Brand, Navbar } from "./Components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
